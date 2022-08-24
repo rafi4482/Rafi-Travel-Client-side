1873ae7a18f719004f29301c544d91cde18373fa
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2022-08-24 09:11:12 +0600

    new

--- src/Foot/Foot.js
+++ src/Foot/Foot.js
@@ -5,22 +5,24 @@ import { Col, Container, Row } from 'react-bootstrap';
 const Foot = () => {
     return (
         <>
-        <div className="footer-design">
-        <Container>
-  <Row>
-    <Col>Rafi Travel
-    <br />
-    Address:58 Murray King Road,Budapest,Hungary
-    </Col>
-    <Col>CELL:+123-445-7778
-    <br />
-    EMAIL:
-    <a href="">abcde@gmail.com</a>
-  
-    </Col>
-  </Row>
-</Container>
+        
+      <div className="footer-design">
+
+<div className="footer-content">
+            <h3>RAFI TRAVEL</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo iste corrupti doloribus odio sed!</p>
+            <ul className="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
         </div>
+        <div className="footer-bottom">
+            <p>copyright &copy;2020 </p>
+        </div>
+      </div>  
             
         </>
  );
